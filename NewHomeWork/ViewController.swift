369ad83088ec9f47063be1adf8c85583e0c6c8d7
tatina140810 <tatu140810tatu@gmail.com>
2024-03-20 19:38:17 +0600

    Initial Commit

---
+++ NewHomeWork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewHomeWork
+//
+//  Created by Tatina Dzhakypbekova on 20/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
